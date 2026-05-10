47161f24feddbc98c0b5733633905b60d583f95d
pr31988 <pr31988@gmail.com>
2026-05-10 21:25:16 +0530

    Configure Terraform with Azure backend and provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 
+ backend "azurerm" {
+    resource_group_name  = "weather-rg"
+    storage_account_name = "weathertfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
 provider "azurerm"{
    features {}
 }
