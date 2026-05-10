35b6e0d48b412a63ea7ee875b29f01f39b6c08ce
pr31988 <pr31988@gmail.com>
2026-05-10 22:16:22 +0530

    Fix typos in main.tf for resource locations

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_container_registry" "acr" {
     name = "weatheracr123"
     resource_group_name = azurerm_resource_group.rg.name
-    loaction = azurerm_resource_group.rg.location
+    location = azurerm_resource_group.rg.location
     sku = "basic"
     admin_enabled = "true"
 }
@@ -35,15 +35,15 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_service_plan" "plan" {
     name = "weather-plan"
     resource_group_name = azurerm_resource_group.rg.name
-    location = azurerrm_resource_group.rg.location
+    location = azurerm_resource_group.rg.location
     os_type = "Linux"
     sku_name = "B1"
 }
 
-resource "azurerrm_linux_web_app" "app" {
+resource "azurerm_linux_web_app" "app" {
      name = "weather-app"
      resource_group_name = azurerm_resource_group.rg.name
-     location = azurerrm_resource_group.rg.location
+     location = azurerm_resource_group.rg.location
      service_plan_id = azurerm_service_plan.plan.id
 
      site_config {
@@ -57,6 +57,6 @@ resource "azurerrm_linux_web_app" "app" {
 
   resource "azurerm_role_assignment" "acr_pull" {
       principal_id = azurerm_linux_web_app.app.identity.principal_id
-      role_definition_name = Acrpull"
+      role_definition_name = "AcrPull"
       scope = azurerm_container_registry.acr.id
   }
