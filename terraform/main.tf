ea982ded79e8d87da2f09e124d12f136c07283c1
pr31988 <pr31988@gmail.com>
2026-05-11 09:37:51 +0530

    Rename weather app to weather-app1988
    web app name should be unique

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_service_plan" "plan" {
 }
 
 resource "azurerm_linux_web_app" "app" {
-     name = "weather-app"
+     name = "weather-app1988"
      resource_group_name = azurerm_resource_group.rg.name
      location = azurerm_resource_group.rg.location
      service_plan_id = azurerm_service_plan.plan.id
