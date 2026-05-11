1af9ef856dcb8ad8aa908bd4c677f35ce6abe71f
pr31988 <pr31988@gmail.com>
2026-05-11 09:58:01 +0530

    Comment out ACR pull role assignment resource
    Comment out azurerm_role_assignment resource for ACR pull.

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,8 +55,8 @@ resource "azurerm_linux_web_app" "app" {
      } 
   }
 
-  resource "azurerm_role_assignment" "acr_pull" {
-      principal_id = azurerm_linux_web_app.app.identity[0].principal_id
-      role_definition_name = "AcrPull"
-      scope = azurerm_container_registry.acr.id
-  }
+  #resource "azurerm_role_assignment" "acr_pull" {
+  #    principal_id = azurerm_linux_web_app.app.identity[0].principal_id
+  #    role_definition_name = "AcrPull"
+  #    scope = azurerm_container_registry.acr.id
+  #}
