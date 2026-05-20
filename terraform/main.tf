cfde9b23238d6d60b9e155c91d8cd2ac73b86ff6
pr31988 <pr31988@gmail.com>
2026-05-20 15:08:07 +0530

    Enable ACR pull role assignment for web app
    Uncommented and updated azurerm_role_assignment resource for ACR pull access.

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,8 +60,14 @@ resource "azurerm_linux_web_app" "app" {
      }
   }
 
-  #resource "azurerm_role_assignment" "acr_pull" {
-  #    principal_id = azurerm_linux_web_app.app.identity[0].principal_id
-  #    role_definition_name = "AcrPull"
-  #    scope = azurerm_container_registry.acr.id
-  #}
+  resource "azurerm_role_assignment" "acr_pull" {
+      role_definition_name = "AcrPull"
+      scope = azurerm_container_registry.acr.id
+
+      principal_id = azurerm_linux_web_app.app.identity[0].principal_id
+
+      skip_service_principal_aad_check = true
+      depends_on = [
+        azurerm_linux_web_app.app
+      ]    
+  }
